e5ccdef8a0a690b60804148acb9f52a5ca89a3b1
rxb3rth <reliutg@gmail.com>
2023-01-30 23:15:29 +0100

    first commit

---
+++ lib/BuzzNotifyContainer.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { inject, onMounted, ref } from "vue";
+import { optionsKey } from "./main";
+import "@reliutg/buzz-notify/dist/buzz-notify.css";
+
+const options = inject(optionsKey);
+
+const notify = ref(null);
+
+const defineOptionsAttrs = (options, el) => {
+  for (const key in options) {
+    const value = options[key];
+    el.setAttribute(`data-notify-${key}`, value);
+  }
+};
+
+onMounted(() => {
+  defineOptionsAttrs(options, notify.value);
+});
+</script>
+
+<template>
+  <div ref="notify" data-notify></div>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.js"),
+      name: "BuzzNotify",
+      // the proper extensions will be added
+      fileName: "vue-buzz-notify",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
